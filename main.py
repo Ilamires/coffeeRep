7b0cdb3843f69735c87445dd8b82ee886698112d
vlad <v.shamraev1@yandex.ru>
2021-11-24 18:35:23 +0300

    Create coffee

--- main.py
+++ main.py
@@ -1,39 +1,44 @@
-import random
 import sys
+import sqlite3
 
 from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QWidget
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.btn.clicked.connect(self.paint)
-        self.do_paint = False
+        uic.loadUi('main.ui', self)
+        con = sqlite3.connect("coffee.sqlite")
+        cur = con.cursor()
 
-    def paintEvent(self, event):
-        if self.do_paint:
-            qp = QPainter()
-            qp.begin(self)
-            self.CreateYellowEllipse(qp)
-            qp.end()
+        self.result = cur.execute("""SELECT varieties,roast,groundORInGrains,taste,price,volume
+                                    FROM coffees""").fetchall()
+        self.page = 1
+        self.pages = len(self.result) // 5 + 1
+        print(self.result)
+        self.Change()
+        con.close()
 
-    def paint(self):
-        self.do_paint = True
-        self.repaint()
+    def ChangePage(self):
+        if self.sender().text() == '>' and self.page < self.pages:
+            self.page += 1
+        elif self.page > 1:
+            self.page -= 1
+        self.Change()
 
-    def CreateYellowEllipse(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
-        f = random.randint(30, 70)
-        qp.drawEllipse(random.randint(50, 300), random.randint(50, 300), f, f)
-        f = random.randint(30, 70)
-        qp.drawEllipse(random.randint(50, 300), random.randint(50, 300), f, f)
-        f = random.randint(30, 70)
-        qp.drawEllipse(random.randint(50, 300), random.randint(50, 300), f, f)
-        f = random.randint(30, 70)
-        qp.drawEllipse(random.randint(50, 300), random.randint(50, 300), f, f)
+    def Change(self):
+        for i in range(5):
+            if i <= len(self.result) - 1:
+                eval(f"self.lbl1_{i + 1 + (self.page - 1) * 5}.setText('{self.result[i][0]}')")
+                eval(f"self.lbl2_{i + 1 + (self.page - 1) * 5}.setText('{self.result[i][1]}')")
+                eval(f"self.lbl3_{i + 1 + (self.page - 1) * 5}.setText('{self.result[i][2]}')")
+                eval(f"self.lbl4_{i + 1 + (self.page - 1) * 5}.setText('{self.result[i][3]}')")
+                eval(f"self.lbl5_{i + 1 + (self.page - 1) * 5}.setText('{self.result[i][4]}')")
+                eval(f"self.lbl6_{i + 1 + (self.page - 1) * 5}.setText('{self.result[i][5]}')")
+            else:
+                break
 
 
 if __name__ == '__main__':
